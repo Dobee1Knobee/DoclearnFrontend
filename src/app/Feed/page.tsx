17d6e5a6ddd8e0a7bd9d446fdf75e16da283be4c
NeGaTiVe357 <kuzinartem74@gmail.com>
2025-03-01 15:14:01 +0300

    тест4

--- src/app/Feed/page.tsx
+++ src/app/Feed/page.tsx
@@ -2,7 +2,7 @@ export default function FeedPage() {
     return (
       <div className="container mx-auto px-4 py-8">
         <h1 className="text-3xl font-bold mb-4" style={{marginTop:"10rem"}}>Лента постов</h1>
-        <p>Здесь будет отображаться лента постов от пользователей. Скоро будет реализована</p>
+        <p>Здесь будет отображаться лента постов от пользователей. Скоро будет реализована. Это новая страница</p>
       </div>
     )
   }
